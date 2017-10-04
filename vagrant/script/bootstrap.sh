1cd45213b37daa53e30b8bf095a49e0548cd1563
Natapone C <natapone@gmail.com>
2017-10-04 09:45:57 +0700

    pre install script Etherium

---
+++ vagrant/script/bootstrap.sh
@@ -0,0 +1,11 @@
+apt-get update --force-yes
+apt-get -f install
+apt-get install -y curl
+
+# # Install Etherium
+# apt-get -f install software-properties-common
+# add-apt-repository -y ppa:ethereum/ethereum
+# apt-get update --force-yes
+# apt-get install -y ethereum
+# apt-get install -y ntp # Automatic time from the Internet
+# service ntp start
