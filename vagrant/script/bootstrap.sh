2f3b332d763648a3e542517100c98ef5fd50bae3
Natapone Charsombut <natapone@gmail.com>
2019-06-19 14:11:41 +0630

    update Ubuntu version

--- vagrant/script/bootstrap.sh
+++ vagrant/script/bootstrap.sh
@@ -1,6 +1,13 @@
 apt-get update --force-yes
 apt-get -f install
 apt-get install -y curl
+apt-get install -y unzip
+
+# fix local
+echo -e 'LANGUAGE=en_US.UTF-8
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8' > /etc/default/locale
 
 # # Install Etherium
 # apt-get -f install software-properties-common
