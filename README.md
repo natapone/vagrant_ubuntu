faa19a3b8e6429c0c0eed0b7040d36f722d7798c
Natapone Charsombut <natapone@gmail.com>
2022-10-23 16:05:10 +0700

    ubuntu 22.04

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vagrant_ubuntu
-Vagrant box for Perl development on Ubuntu 14.04
+Vagrant box for Perl development on Ubuntu 22.04
--- vagrant/script/bootstrap.sh
+++ vagrant/script/bootstrap.sh
@@ -1,18 +1,3 @@
-apt-get update --force-yes
-apt-get -f install
+apt-get update -y
+apt-get upgrade -y
 apt-get install -y curl
-apt-get install -y unzip
-
-# fix local
-echo -e 'LANGUAGE=en_US.UTF-8
-LANG=en_US.UTF-8
-LC_ALL=en_US.UTF-8
-LC_CTYPE=en_US.UTF-8' > /etc/default/locale
-
-# # Install Etherium
-# apt-get -f install software-properties-common
-# add-apt-repository -y ppa:ethereum/ethereum
-# apt-get update --force-yes
-# apt-get install -y ethereum
-# apt-get install -y ntp # Automatic time from the Internet
-# service ntp start
